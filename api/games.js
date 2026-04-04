fa1f872ebf3960d9e76a2e93845dfb28d13b7002
Chaitanya <74352886+CHAITANYA13-HARI@users.noreply.github.com>
2026-04-04 14:34:46 +0530

    Update games.js

--- api/games.js
+++ api/games.js
@@ -4,13 +4,13 @@ export default function handler(req, res) {
       id: 1,
       name: "2048",
       url: "https://chaitanya13-hari.github.io/2048/",
-      thumbnail: "https://via.placeholder.com/150?text=2048"
+      thumbnail: "https://games.washingtonpost.com/arenaxstorage-blob/arenax-games/TwoZeroFourEight/0.37/assets/thumbs/thumb2x.jpg"
     },
     {
       id: 2,
       name: "Snake Game",
       url: "https://chaitanya13-hari.github.io/snake/",
-      thumbnail: "https://via.placeholder.com/150?text=Tap"
+      thumbnail: "https://tse2.mm.bing.net/th/id/OIP.EGujGmjHsKBeLJFeYcPONgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3"
     }
   ];
 
